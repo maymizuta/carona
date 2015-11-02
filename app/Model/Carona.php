c183923348621866b98f7353436c86e5a382c2b3
Guilherme <guitimoteo@yahoo.com.br>
2015-11-02 17:56:09 -0200

    Adicionando hasMany no pedido.

--- app/Model/Carona.php
+++ app/Model/Carona.php
@@ -1,16 +1,26 @@
 <?php
+
 App::uses('AppModel', 'Model');
+
 /**
  * Carona Model
  *
  */
 class Carona extends AppModel {
 
-/**
- * Use database config
- *
- * @var string
- */
-	public $useDbConfig = 'caronasolidaria';
+    /**
+     * Use database config
+     *
+     * @var string
+     */
+    public $useDbConfig = 'caronasolidaria';
+    public $hasMany = array(
+        'Pedido' => array(
+            'className' => 'Pedido',
+            'foreignKey' => 'carona_id',
+            'conditions' => '',
+            'fields' => array('id'),
+            'order' => ''
+    ));
 
 }
