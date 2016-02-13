f8ad5f9e54ff291e72cbdd0645bf2b2a76903b88
Guilherme <guitimoteo@yahoo.com.br>
2016-02-13 17:52:15 -0200

    Trivial: indentação

--- app/Model/Pedido.php
+++ app/Model/Pedido.php
@@ -1,5 +1,7 @@
 <?php
+
 App::uses('AppModel', 'Model');
+
 /**
  * Pedido Model
  *
@@ -8,35 +10,36 @@ App::uses('AppModel', 'Model');
  */
 class Pedido extends AppModel {
 
-/**
- * Use database config
- *
- * @var string
- */
-	public $useDbConfig = 'caronasolidaria';
+    /**
+     * Use database config
+     *
+     * @var string
+     */
+    public $useDbConfig = 'caronasolidaria';
 
 
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
+    //The Associations below have been created with all possible keys, those that are not needed can be removed
 
-/**
- * belongsTo associations
- *
- * @var array
- */
-	public $belongsTo = array(
-		'User' => array(
-			'className' => 'User',
-			'foreignKey' => 'user_id',
-			'conditions' => '',
-			'fields' => array('email','id','nome'),
-			'order' => ''
-		),
-		'Carona' => array(
-			'className' => 'Carona',
-			'foreignKey' => 'carona_id',
-			'conditions' => '',
-			'fields' => array('id', 'user_id'),
-			'order' => ''
-		)
-	);
+    /**
+     * belongsTo associations
+     *
+     * @var array
+     */
+    public $belongsTo = array(
+        'User' => array(
+            'className' => 'User',
+            'foreignKey' => 'user_id',
+            'conditions' => '',
+            'fields' => array('email', 'id', 'nome'),
+            'order' => ''
+        ),
+        'Carona' => array(
+            'className' => 'Carona',
+            'foreignKey' => 'carona_id',
+            'conditions' => '',
+            'fields' => array('id', 'user_id'),
+            'order' => ''
+        )
+    );
+
 }
